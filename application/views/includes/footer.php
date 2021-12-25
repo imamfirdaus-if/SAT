ec39c95d5d99131b5c8b6a6d00ce3d16bfc80774
imamfirdaus-if <imamfirdaus3110@gmail.com>
2021-12-25 12:07:53 +0700

    first commit

---
+++ application/views/includes/footer.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+<strong>Copyright &copy; 2021 Imam Firdaus - 1197050051.</strong>
+All rights reserved.
+<div class="float-right d-none d-sm-inline-block">
+  <b>IFTECH</b> 
+</div>
+</footer>
